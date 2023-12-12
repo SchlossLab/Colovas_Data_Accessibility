55adfb88d011d237795af55295b2531b3c0b0373
Joanna Colovas <joannacolovas@gmail.com>
2023-12-12 16:30:43 -0500

    update read csv and count of papers

--- Notebook/20231128_PaperDataCleanup.R
+++ Notebook/20231128_PaperDataCleanup.R
@@ -40,6 +40,9 @@ ac_500_papers <- full_join(manually_assessed_papers, seq_papers, by = c("paper",
 ac_500_papers %>% count(new_seq_data, availability)
 
 write_csv(ac_500_papers, file = "ac_papers_tocheck.csv")
+ac_papers_tocheck <- read_csv("ac_papers_tocheck.csv") 
+
+ac_papers_tocheck %>% count(new_seq_data, availability)
 #using seq_papers dataframe, arrange by link, and remove duplicates
 
 seq_papers_byLink <- arrange(seq_papers, paper) %>% 
