e51596948a99043edb2e8e2653db769e15d4e76d
Joanna Colovas <joannacolovas@gmail.com>
2023-12-14 15:21:53 -0500

    move files, create new ground truth file, updated PaperDataCleanup file with only used data frames

--- Notebook/20231128_PaperDataCleanup.R
+++ Notebook/20231128_PaperDataCleanup.R
@@ -5,13 +5,14 @@
 #20231130 Replicate creation of papers dataframe from data from google drive file 
 #https://docs.google.com/spreadsheets/d/1D9lx5gpamhryRRdo7mZrlEzLMfOTFN0rT5UnR3a9omo/edit?usp=sharing
 
+# 202312-- merge data frame seq_papers(500 papers) from AC .RData file seq_papers_202340505.RData with
+# dataframe manually_assessed_papers, constructed from google sheet above
+
 #load necessary packages
 library(tidyverse)
 library(purrr)
 library(tidytext)
 
-#set working directory to directory "Colovas_Data_Accessibility"
-#setwd("~/Documents/Schloss/Colovas_Data_Accessibility")
 
 #from downloaded google spreadsheet of manually assessed ASM manuscripts, create a dataframe of manually assessed papers
 
@@ -23,13 +24,6 @@ manually_assessed_papers <- read_csv("Adena_Stuff/ASMSequencingPaperResponses.cs
 
 load("~/Documents/Schloss/Colovas_Data_Accessibility/Adena_Stuff/seq_papers_20230505.RData")
 
-#add column with clear statement of data availability
-manually_assessed_papers <- 
-  mutate(manually_assessed_papers, data_available = ifelse(availability != "No", "Data Available", "No Data Available") )
-
-#clean dataframe manually_assessed_papers to exclude duplicates
-manual_papers_byLink <- arrange(manually_assessed_papers, paper) %>% 
-  distinct(paper, .keep_all = TRUE)
 
 #find out which papers from seq_papers and manually_assessed_papers are the same, and which are not
 seq_papers <- seq_papers %>% distinct(paper, .keep_all = TRUE)
@@ -39,37 +33,23 @@ ac_500_papers <- full_join(manually_assessed_papers, seq_papers, by = c("paper",
 #count data
 ac_500_papers %>% count(new_seq_data, availability)
 
+
+#write files to CSV to continue checking papers for data availability
 write_csv(ac_500_papers, file = "ac_papers_tocheck.csv")
-ac_papers_tocheck <- read_csv("ac_papers_tocheck.csv") 
+
+#read in finished csv file to manipulate and continue cleaning columns 
+ac_papers_tocheck <- read_csv("Data/ac_papers_tocheck.csv") 
 
 ac_papers_tocheck %>% count(new_seq_data, availability)
-#using seq_papers dataframe, arrange by link, and remove duplicates
-
-seq_papers_byLink <- arrange(seq_papers, paper) %>% 
-  distinct(doi, .keep_all = TRUE)
-
-#using seq_papers_texts dataframe to pull HTML text, arrange by link, and remove duplicates
-papers_HTML_byLink <-  arrange(seq_papers_texts, paper) %>% 
-  distinct(paper, .keep_all = TRUE)
-
-#join all 3 dataframes for the most complete record of data for N=192 papers
-
-papers_noDupes_allVars <-  inner_join(manual_papers_byLink, seq_papers_byLink, papers_HTML_byLink, by = "paper") %>% tibble()
-
-#count the number of papers containing new sequence data (No-97, Yes-95, N=192)
-
-count(papers_noDupes_allVars, new_seq_data.x)
-
-#make sure dataframe is grouped by journal title (container.title)
-papers_noDupes_allVars %>% 
-  group_by(container.title) %>% 
-  count()
-
-#use mutate to add data availability column to show text "Data Available" or "Data Not Available" 
-#papers_noDupes_allVars <- 
- # mutate(papers_noDupes_allData_allLinks, data_available = ifelse(new_seq_data == "Yes", "Data Available", "No Data Available") )
 
 
+#add column with clear statement of data availability for all data in column "data_available"
+ac_papers_tocheck <- 
+  mutate(ac_papers_tocheck, data_available = ifelse(availability != "No", "Data Available", "No Data Available") )
 
-#now i need to figure out where the text of each paper is located, or how to pull it based on Adena's code
-#the text of each paper is in dataframe seq_papers_texts with 500 observations of 2 variables, paper and text
+#move papers column to the beginning of the dataset
+ac_papers_tocheck <- 
+  ac_papers_tocheck %>% relocate(paper)
+
+#write csv for trusted "ground truth" file
+write_csv(ac_papers_tocheck, file = "Data/groundtruth.csv")
