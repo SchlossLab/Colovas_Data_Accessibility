99c27aa29e91562165e84889090a778bd6ff3c36
Joanna Colovas <joannacolovas@gmail.com>
2024-01-04 15:47:08 -0500

    updated text scraping example/practice

--- TextScraping.R
+++ TextScraping.R
@@ -6,3 +6,27 @@ library(polite)
 groundtruth <- read_csv("Data/groundtruth.csv")
 
 
+read_html("test.html") %>% html_text()
+
+read_html("test.html") %>% html_text2()
+
+
+#example paper https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003 
+#Persistent Bacteremia in Rabbit Fetuses despite Maternal Antibiotic Therapy in a Novel Intrauterine-Infection Model
+
+#this doesn't get the abstract of the paper, do we want it separate or together?
+
+test_abstract <- read_html("https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003") %>%
+  html_elements("section#abstract") %>%
+  html_elements("[role = paragraph]")
+
+test_abstract_text <- html_text(test_abstract)
+
+test_body <- read_html("https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003") %>%
+  html_elements("section#bodymatter") %>%
+  html_elements("[role = paragraph]")
+
+test_body_text <- html_text(test_body)
+
+all_text <- c(test_abstract_text, test_body_text)
+all_text_string <- paste(all_text, collapse = " ")
