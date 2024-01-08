d2d996c2cf136c41d8093905d6c7e733b6c4c3f6
Joanna Colovas <joannacolovas@gmail.com>
2024-01-08 16:40:14 -0500

    attempt at polite "bow" for webscrape (fail)

--- TextScraping.R
+++ TextScraping.R
@@ -6,9 +6,9 @@ library(polite)
 groundtruth <- read_csv("Data/groundtruth.csv")
 
 
-read_html("test.html") %>% html_text()
+#read_html("test.html") %>% html_text()
 
-read_html("test.html") %>% html_text2()
+#read_html("test.html") %>% html_text2()
 
 
 #example paper https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003 
@@ -16,13 +16,21 @@ read_html("test.html") %>% html_text2()
 
 #this doesn't get the abstract of the paper, do we want it separate or together?
 
-test_abstract <- read_html("https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003") %>%
+test_url <- "https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003"
+
+#use polite package to introduce yourself to the web host
+
+bow(test_url,
+    user_agent = "University of Michigan Resercher, joannacolovas@gmail.com", 
+    verbose = TRUE)
+
+test_abstract <- read_html(test_url, verbose = TRUE) %>%
   html_elements("section#abstract") %>%
   html_elements("[role = paragraph]")
 
 test_abstract_text <- html_text(test_abstract)
 
-test_body <- read_html("https://journals.asm.org/doi/10.1128/aac.47.7.2125-2130.2003") %>%
+test_body <- read_html(test_url) %>%
   html_elements("section#bodymatter") %>%
   html_elements("[role = paragraph]")
 
