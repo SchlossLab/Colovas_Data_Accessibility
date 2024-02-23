774461933392828db8796d1eba68d4793b8fcf6a
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-02-23 12:26:56 -0500

    Creating a pipeline for the creation of json files

--- pipelineTest_GL20240223.R
+++ pipelineTest_GL20240223.R
@@ -1,6 +1,6 @@
 # GL test script for smaller subset (N=30) of groundtruth (gt_subset_30)
-#goals: read html, removal of figures and tables, save html, tokenize paper, send each thing to a json file
-
+# goals: read html, removal of figures and tables, save html, tokenize paper, send each thing to a json file
+#
 #library statements
 library(tidyverse)
 library(rvest)
@@ -8,9 +8,9 @@ library(tidytext)
 library(tibble)
 library(xml2)
 library(jsonlite)
-
 #load data
-gt_subset_30 <- read_csv("Data/gt_subset_30.csv")
+
+
 
 #function for reading html, remove figs/tables, and concatenate abstract and body (using rvest, xml2)
 webscrape <- function(doi) {
@@ -41,13 +41,13 @@ webscrape <- function(doi) {
 #here will need map statement to webscrape for each doi, and then a mutate to add it to the df as a column with the text in it
 #does it need a map or just a mutate? 
 
-mutate(gt_subset_30, 
-       paper_html = webscrape(gt_subset_30$paper))
+# mutate(data,
+#        paper_html = webscrape(data$paper))
 
 
 #Function for token counting pipeline (from TidyingText.R)
-count_words <- function(file_name, min_word_length = 3) {
-  readLines(file_name) %>% 
+create_tokens <- function(html_text, min_word_length = 3) {
+  html_text %>%
     as_tibble() %>% 
     unnest_tokens(word, value) %>% 
     arrange() %>% 
@@ -55,3 +55,29 @@ count_words <- function(file_name, min_word_length = 3) {
     anti_join(., stop_words, by = "word") %>% 
     count(word)
 }
+
+prepare_data <- function(data, file_path = "Data/data.json", n_tokens = 1)
+{
+  json_data <- 0
+  df <- 0
+  if(file.exists(file_path))
+  {
+    json_data <- fromJSON(file_path)
+    tibble_data <- map(json_data$html_text,
+                       create_tokens)
+    df <- data.frame(json_data$paper, json_data$html_text, tibble_data)
+  }
+  else
+  {
+    webscraped_data <- map(data$paper, webscrape)
+    tibble_data <- map(webscraped_data, create_tokens)
+    df <- data.frame(data$paper, webscraped_data
+    , tibble_data)
+  }
+  json_data <- toJSON(df)
+  save(json_data, file=file_path)
+  return (json_data)
+}
+
+gt_subset_30 <- read_csv("Data/gt_subset_30.csv")
+prepare_data(gt_subset_30)
