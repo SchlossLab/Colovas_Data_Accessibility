4f37f185441d600ecb627682489b78d2fea2debf
Joanna Colovas <joannacolovas@gmail.com>
2024-04-16 12:38:17 -0400

    fixed unnesting (AGAIN)

--- TidyModels.R
+++ TidyModels.R
@@ -5,7 +5,8 @@
 library(tidymodels)
 library(jsonlite)
 library(textrecipes)
-library(spacyr)
+library(spacyr) #for lemmatization 
+
 #may or may not need the following packages, IDK yet
 #library(discrim) #may need for bayes model
 #library(broom.mixed) #converting bayesian models to tidy tibble
@@ -15,19 +16,28 @@ library(spacyr)
 #library(vip) #may need for visualization?
 
 
+
 #read and unserialize json file (gtss30)
 jsonfile <- "Data/gt_subset_30_data.json"
 json_data <- read_json(jsonfile)  
 json_data <- unserializeJSON(json_data[[1]])
 
+
+
 #set up dataset for 1 ML model; paper, new_seq_data, text_tibble
 json_tibble <- tibble(paper_doi = json_data$`data$paper`,
                       new_seq_data = json_data$`data$new_seq_data`,
                       availability = json_data$`data$availability`,
-                      paper_html = json_data$webscraped_data)
+                      paper_html = json_data$`webscraped_data`)
 
-json_tibble <- unnest(json_tibble, col = paper_html) %>%
-  rename(., paper_html = ".")
+#fixed unnesting 20240416
+json_tibble <- unnest_wider(json_tibble, paper_html, names_sep = "") %>% 
+  unnest_wider(paper_html., names_sep = "") %>% 
+  mutate(paper_html = paste0(paper_html.1, paper_html.2)) %>% 
+  select(c(-paper_html.1, -paper_html.2))
+
+#check status
+str(json_tibble)
 
 
 #set seed
@@ -47,7 +57,7 @@ gt_test <- testing(data_split)
 gt_recipe <- 
   recipe(new_seq_data ~ paper_html, data = gt_train) %>% 
   step_tokenize(paper_html) %>% 
- # step_stem(paper_html) %>% 
+  step_lemma(paper_html) %>% 
   step_stopwords(paper_html) %>% 
   step_ngram(paper_html, min_num_tokens = 1, num_tokens = 3) %>% 
   show_tokens(paper_html)
