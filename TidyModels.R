7f202bb3d8ba27a7491271ecf00b648e17cfdec5
Joanna Colovas <joannacolovas@gmail.com>
2024-04-19 16:48:54 -0400

    formatting, resampling and bootstrapping

--- TidyModels.R
+++ TidyModels.R
@@ -18,6 +18,7 @@ library(spacyr) #for lemmatization
 #library(rpart.plot) #may need for visualization?
 #library(vip) #may need for visualization?
 
+#------------------dataset prep and formatting-----------------------
 
 #read and unserialize json file (gtss30)
 jsonfile <- "Data/gt_subset_30_data.json"
@@ -37,11 +38,13 @@ json_tibble <- unnest_wider(json_tibble, paper_html, names_sep = "") %>%
   mutate(paper_html = paste0(paper_html.1, paper_html.2)) %>% 
   select(c(-paper_html.1, -paper_html.2))
 
-#check status
+#check status to make sure column is not still a list
 str(json_tibble$paper_html)
 
 #remove HTML tags, punctuation, digits from text 
 #tokenize using hunspell with format = html and then paste
+#is there a better way to remove this and 
+#actually get rid of the space characters? >> for mob code weds? 
 prep_html <- function(html) {
   tokens <- hunspell_parse(html, format = "html") %>%  
     unlist() %>% 
@@ -51,6 +54,7 @@ prep_html <- function(html) {
 
 json_tibble$paper_html <- map_chr(json_tibble$paper_html, prep_html)
 
+#------------------split and nesting of data/folds--------------------
 
 #set seed
 set.seed(102899)
@@ -60,16 +64,34 @@ gt_data <- json_tibble %>%
   mutate(new_seq_data = factor(new_seq_data), 
          availability = factor(availability))
 
+
 #initial split of data into training and test data 
+#20240419- i think i still need this even though it's a nested sample
 data_split <- initial_split(gt_data, strata = new_seq_data)
 gt_train <- training(data_split)
 gt_test <- testing(data_split)
 
+#nested resampling of data using methods from mikropml
+#20240419- can we specify what proportion goes into each re-sample? ie 80/20?
+nested_resample <- nested_cv(gt_train, 
+                             outside = vfold_cv(repeats = 10, strata = new_seq_data), 
+                             inside = bootstraps(times = 10))
+nested_resample
 
-#begin recipes
+
+#---------------------modeling-------------------------------
+
+#------recipes for dataset prep-------
+
 #can tune num_tokens = tune()
+#20240419 - do i need to use data = nested_resample?  
+#OR is there a test/training set to use for this? 
+#20240419 - why can't the recipe find the cols it needs? 
 gt_recipe <- 
-  recipe(new_seq_data ~ paper_html, data = gt_train) %>% 
+  recipe(new_seq_data ~ paper_html, data = nested_resample) %>% 
+  # Do not use paper_doi and availability as predictors
+  update_role(paper_doi, new_role = "id") %>%
+  update_role(availability, new_role = "id") %>%
   step_tokenize(paper_html, engine = "spacyr") %>% 
   step_stopwords(paper_html, stopword_source = "smart") %>% 
   step_lemma(paper_html) %>% 
@@ -78,5 +100,5 @@ gt_recipe <-
 
 head(gt_recipe, 2) 
   
-
+#-----------model(s)------------
 
