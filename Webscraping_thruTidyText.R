b2eeb8cc3f3ab5f69c348a5a8957e4b79cbf20a3
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-05-07 12:50:53 -0400

    tried to submit on cluster, issue with read_xml, i think in prep html fxn

--- Webscraping_thruTidyText.R
+++ Webscraping_thruTidyText.R
@@ -10,6 +10,7 @@ library(xml2)
 library(jsonlite)
 library(textstem) #for stemming text variables
 library(tm) #for text manipulation
+library(tokenizers) #for text tokenization
 
 #load data
 
@@ -35,7 +36,7 @@ webscrape <- function(doi) {
     html_children() %>%
     xml_remove()
   
-  paper_html <- paste(abstract, body) %>% tibble()
+  paper_html <- paste0(abstract, body) %>% tibble()
   
   return(paper_html)
   
@@ -64,10 +65,8 @@ prep_html_tm <- function(html) {
 #function for creating json file of data
 #add year published, and journal name 
 prepare_data <- function(data, file_path){
-  if (webscrape_data == FALSE){
-    webscraped_data <- lapply(data$paper, webscrape)
-  }
   
+  webscraped_data <- lapply(data$paper, webscrape)
   clean_text <- lapply(webscraped_data, prep_html_tm)
   paper_tokens <- lapply(clean_text, tokenize_ngrams, n_min = 1, n = 3,
                          stopwords = stopwords::stopwords("en"))
@@ -76,10 +75,10 @@ prepare_data <- function(data, file_path){
   if (data$year.published == FALSE) {
     mutate(year.published = case_when(
       str_detect(published.print, "/") ~ str_c("20", str_sub(published.print, start = -2, -1)), 
-      str_detect(published.print, "-") ~ substring(published.print, 1, 4) )) 
+      str_detect(published.print, "-") ~ substring(published.print, 1, 4))) 
   }
   
-  df <- lst(paper_doi = data$paper, data$new_seq_data, data$availability, 
+  df <- lst(paper_doi = data$paper, data$new_seq_data, data$availability,
             paper_html = webscraped_data, clean_text, paper_tokens, unlisted_tokens,
             journal = data$container.title, year_published = data$year.published)
   
@@ -93,6 +92,8 @@ prepare_data <- function(data, file_path){
 
 gt_ss30 <- read_csv("Data/gt_subset_30.csv")
 prepare_data(gt_ss30, "Data/gt_subset_30_data.json")
+
+
 
 # groundtruth <- read_csv("Data/groundtruth.csv")
 # prepare_data(groundtruth, "Data/groundtruth.json")
