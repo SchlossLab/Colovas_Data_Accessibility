0a90bcf6ac4925a31b0848ef0c58866fe1a67492
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-05-16 17:03:54 -0400

    save gtss30 webscrape as csv.gz

--- Code/Webscrape.R
+++ Code/Webscrape.R
@@ -32,7 +32,7 @@ webscrape <- function(doi) {
     html_children() %>%
     xml_remove()
   
-  paper_html <- paste0(abstract, body) %>% tibble()
+  paper_html <- paste0(abstract, body, collapse = " ") 
   
   return(paper_html)
   
@@ -40,11 +40,31 @@ webscrape <- function(doi) {
 
 webscrape_save_html <- function(data, file_path_gz){
 
-  webscraped_data <- lapply(data$paper, webscrape)
+  webscraped_data <-  map_chr(data$paper, webscrape)
 
-  df <- tibble(paper_doi = data$paper,
-             paper_html = webscraped_data)
+  df <- tibble::tibble(paper_doi = data$paper, 
+                      paper_html = webscraped_data)
 
-  csv_df <- write.csv(df, file = file_path_gz, 
-                    col.names = TRUE, row.names = FALSE)
+  write.csv(df, file = file_path_gz, row.names = FALSE)
 }
+
+
+# call function on small dataset
+
+#gt_ss30 <- read_csv("Data/gt_subset_30.csv")
+#webscrape_save_html(gt_ss30, "Data/gt_subset_30_data.csv.gz")
+
+#load csv
+
+#gtss30_webscrape <- read_csv("Data/gt_subset_30_data.csv.gz")
+#head(gtss30_webscrape, 2)
+
+# call function on larger dataset
+
+groundtruth <- read_csv("Data/groundtruth.csv")
+webscrape_save_html(groundtruth, "Data/groundtruth.csv.gz")
+
+#load csv
+
+gt_webscrape <- read_csv("Data/groundtruth.csv.gz")
+head(gt_webscrape, 2)
