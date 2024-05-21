0afbfaaa19dc1028f2574c44c9a8f5951712d98a
Joanna Colovas <joannacolovas@gmail.com>
2024-05-21 15:53:48 -0400

    fix clean html code and run on gt, gtss30

--- Code/cleanHTML.R
+++ Code/cleanHTML.R
@@ -2,11 +2,11 @@
 # take webscraped HTML and clean html tags 
 #
 #
-#
 # load packages
 library(tidyverse)
 library(tidytext)
 library(xml2)
+library(rvest)
 library(textstem) #for stemming text variables
 library(tm) #for text manipulation
 
@@ -20,6 +20,27 @@ prep_html_tm <- function(html) {
   html <- lemmatize_strings(html)
 }
 
-webscraped_data <- read.csv(snakemake.input[0])
-clean_text <- lapply(webscraped_data, prep_html_tm)
-write.csv(df, file = snakemake.output[0], row.names = FALSE)
+# 20240521 - need to update this with more accurate snakefiles
+# webscraped_data <- read.csv(snakemake.input[0])
+# clean_text <- lapply(webscraped_data, prep_html_tm)
+# write.csv(df, file = snakemake.output[0], row.names = FALSE)
+
+# ground truth ss30
+webscraped_data <- read.csv("Data/gt_subset_30_data.csv.gz")
+webscraped_data$clean_html <- lapply(webscraped_data$paper_html, prep_html_tm)
+webscraped_data <- select(webscraped_data, !"paper_html")
+webscraped_data$clean_html <- 
+  map_chr(webscraped_data$clean_html, as.character)
+write.csv(webscraped_data, 
+          file = "Data/gt_subset_30_data_clean_html.csv.gz", 
+          row.names = FALSE)
+
+# groundtruth 
+webscraped_data <- read.csv("Data/groundtruth.csv.gz")
+webscraped_data$clean_html <- lapply(webscraped_data$paper_html, prep_html_tm)
+webscraped_data <- select(webscraped_data, !"paper_html")
+webscraped_data$clean_html <- 
+  map_chr(webscraped_data$clean_html, as.character)
+write.csv(webscraped_data, 
+          file = "Data/groundtruth_clean_html.csv.gz", 
+          row.names = FALSE)
