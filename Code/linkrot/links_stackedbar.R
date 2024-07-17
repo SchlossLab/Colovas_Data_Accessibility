8e82e6b23449ecdaaf4b7c1b87724c4ec76d6796
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-07-17 16:47:05 -0400

    all changes

--- Code/linkrot/links_stackedbar.R
+++ Code/linkrot/links_stackedbar.R
@@ -43,7 +43,7 @@ groundtruth <- read_csv("Data/groundtruth.csv")
 #     head()
 
 
-read_csv("Data/linkrot/groundtruth_alllinks.csv.gz") %>%
+alive_by_total <- read_csv("Data/linkrot/groundtruth_alllinks.csv.gz") %>%
         mutate(is_alive = link_status == 200) %>%
         select(paper, link_text, is_alive) %>%
         distinct() %>% 
@@ -53,7 +53,57 @@ read_csv("Data/linkrot/groundtruth_alllinks.csv.gz") %>%
         mutate(n_total = `FALSE` + `TRUE`, 
                 f_alive = `TRUE`/n_total)
 
+alive_by_total %>%
+    ggplot(aes(x = factor(n_total), y = f_alive)) +
+    geom_jitter()
+
+#try and simplify the code, try not to bring in more dfs than you need
+#downside is that we don't have data for papers with 0 links
+#is a paper with 0 links truly "alive/dead"
+#for area plot - think about how you're going to structure the splitting
+#for this we don't have a t/f it's a proportion
+#categorical version of f_alive? all dead, partial, all alive
+# proportion >0.5 mostly active/dead? 
+# f_alive for number of papers
+
+# you have to have all values of true for all values of x
+# can force that with factoring and not dropping in the count
+#companion plot with histogram 
+alive_by_total %>%
+    filter(n_total <= 5) %>%
+    mutate(`TRUE` = factor(`TRUE`, levels = 0:max(`TRUE`))) %>%
+    count(n_total, `TRUE`, .drop = FALSE) %>%
+    mutate(n_fract = n/sum(n), .by = n_total) %>%
+    ggplot(aes(x = n_total, y = n_fract, fill = `TRUE`)) +
+    geom_area()
+
+#histogram 
+#in general probably chop off at 5 links ish 
+alive_by_total %>%
+    select(paper, n_total) %>%
+    distinct() %>%
+    ggplot(aes(x = n_total)) +
+    geom_histogram()
 
 
+alive_by_total %>%
+    filter(n_total <= 2) %>%
+    group_by(n_total, `TRUE`) %>%
+    
+    summarize(n = sum(n_total)) %>%
+    mutate(percentage = n/sum(n)) %>%
+    #bind_rows(tibble(n_total = 1, `TRUE` = 2, n  = 0, percentage = 0)) %>%
+    ggplot(aes(x= n_total, y = percentage, fill = factor(`TRUE`))) +
+    geom_area()
+    
 
+#example code from https://r-graph-gallery.com/136-stacked-area-chart
+# time <- as.numeric(rep(seq(1,7),each=7))  # x Axis
+# value <- runif(49, 10, 100)               # y Axis
+# group <- rep(LETTERS[1:7],times=7)        # group, one shape per group
+# data <- data.frame(time, value, group)
 
+# data <- data  %>%
+#   group_by(time, group) %>%
+#   summarise(n = sum(value)) %>%
+#   mutate(percentage = n / sum(n))
