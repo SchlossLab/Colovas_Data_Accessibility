37095682833ee3ffe67028e53040723487c9ce95
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-07-23 12:12:30 -0400

    working on filtering features for stats sig

--- Code/ml_models_params.R
+++ Code/ml_models_params.R
@@ -15,39 +15,71 @@ one_rds <- readRDS("Data/ml_results/groundtruth/rf/rf.1.data_availability.model.
 two_rds <- readRDS("Data/ml_results/groundtruth/rf/rf.1.new_seq_data.model.RDS")
 
 #20240722 looking for the feature importance table by p value
-one_rds
-str(one_rds)
-head(one_rds$feature_importance$feat, 10)
-head(two_rds$feature_importance, 10)
-two_rds$feature_importance
 
-# one_rds_feats <-
+one_rds_feats <-
 one_rds$feature_importance %>%
-    filter(pvalue <= 0.2)
+    filter(pvalue <= 0.1)
 
+two_rds_feats <-
 two_rds$feature_importance %>%
-    filter(pvalue <= 0.2)
+    filter(pvalue <= 0.1)
+
+feats_list_test <-
+    rbind(one_rds_feats, two_rds_feats)
 
     
 seq_files_list <- list.files(filepath, 
                         pattern = str_glue("new_seq_data.*.RDS"), 
                         full.names = TRUE)
+small_seq_files_list <- head(seq_files_list, 5)
 
 avail_files_list <- list.files(filepath, 
                         pattern = str_glue("data_availability.*.RDS"), 
                         full.names = TRUE)
 
-seq_results <- map(seq_files_list, readRDS)
+    
+# 20240723 - function doesn't work because it doesn't append correctly to add all data together
+get_sig_feats <- function(rds_list, dest, sig) {
+    seq_results <- map(rds_list, readRDS)
+    dest <- tibble()
+    for(x in 1:length(rds_list)) {
+        y <- seq_results[[x]]$feature_importance %>%
+            filter(pvalue <= sig)
+        dest <- rbind(dest, y)
+    }
+
+return(dest)
+
+}
+
+
+# these also don't work right 
+dest_seq <- get_sig_feats(seq_files_list, dest_seq, 0.0005)
+write.csv(dest_seq, file = str_glue("{filepath}/new_seq_data.features.csv"))
+
+dest_avail <- get_sig_feats(avail_files_list, dest_avail, 0.005)
+write.csv(dest_avail, file = str_glue("{filepath}/data_availability.features.csv"))
+
+
+
+
+# seq_feat_test <- for(x in 1:5) {
+#     seq_results[[x]]$feature_importance %>% 
+#         filter(pvalue <= 0.1) %>% 
+#         print()
+#     }
+
+seq_feats_test <- seq_results[[1]]$feature_importance %>%
+    filter(pvalue <= 0.1) 
+y <- bind_rows(seq_feats_test)
+
+seq_results$feature_importance %>% 
+    filter(pvalue <= 0.1) %>% 
+    head(2)
+
 head(seq_results, 2)
 avail_results <- map(avail_files_list, readRDS)
 head(avail_results, 2)
-# let's try and graph these after we read them all in as csvs
-# instead of trying to read in the RDS files
-#this works!!! yay, you just have to graph diff ones ased on which type of results you want to see
-
-seq_combined <- combine_hp_performance(seq_results)
-#seq_combined <- combine_hp_performance(gtss30_RDS)
-head(seq_combined, 2)
 
 
 
