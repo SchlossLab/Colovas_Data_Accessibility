11e67329b0ea8e54213d099f55e44ac014f6da23
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-07-25 16:32:43 -0400

    added line graphs to by year and year status

--- Code/linkrot/links_byyear.R
+++ Code/linkrot/links_byyear.R
@@ -29,11 +29,11 @@ LinksByYear <-
     data = metadatalinks, 
     mapping = aes(x = year.published)
   ) + 
-  geom_bar(stat = "count") +
+  geom_line(stat = "count", size = 1) +
   theme(axis.text.x = element_text(angle = 75, vjust = 1, hjust=1)) +
   labs( y = "Number of Manuscripts Containing Links", 
         x = "Year Published",
-        title = stringr::str_glue("Number of ASM Manuscripts Containing 1+ External Links\nAdded by User (N={sum})")) 
+        title = stringr::str_glue("Number of ASM Manuscripts by Year Containing 1+ External Links\nAdded by User (N={sum})")) 
 LinksByYear
 
 ggsave(LinksByYear, filename = output)
--- Code/linkrot/links_yearstatus.R
+++ Code/linkrot/links_yearstatus.R
@@ -15,29 +15,44 @@ metadatalinks <- read_csv(metadatalinks)
 output <- input[3]
 
 #non-snakemake implementation
-#alllinks <- read_csv("Data/linkrot/groundtruth_alllinks.csv.gz") %>% unique()
-#metadatalinks <- read_csv("Data/linkrot/groundtruth_links_metadata.csv.gz")
+#alllinks <- read_csv("Data/linkrot/groundtruth.alllinks.csv.gz") %>% unique()
+#metadatalinks <- read_csv("Data/linkrot/groundtruth.linksmetadata.csv.gz")
 
 all_with_meta <- left_join(alllinks, metadatalinks, 
                             by = join_by("paper" == "paper_doi"),
                             relationship = "many-to-one")
 
 year_tally <- all_with_meta %>% 
-                  group_by(year.published, link_status) %>% 
+                  group_by(year.published, binary_status) %>% 
                   tally()
 sum <- as.numeric(sum(year_tally$n)) 
 
+#as bargraph
 LinksByYearAndStatus <- 
   ggplot(
     data = unique(all_with_meta), 
-    mapping = aes(x = year.published, fill = as.factor(link_status))
-  ) + 
+    mapping = aes(x = year.published, fill = binary_status)) + 
   geom_bar(stat = "count") +
   theme(axis.text.x = element_text(angle = 75, vjust = 1, hjust=1)) +
   labs( y = "Number of Manuscripts Containing Links", 
         x = "Year Published",
         title = stringr::str_glue("Number of External User-Added Links by Year and Status (N={sum})"), 
-        fill = "Link Status") 
+        fill = "Link Status") +
+scale_fill_manual(values = c("seagreen2", "indianred1"))
 LinksByYearAndStatus
 
 ggsave(LinksByYearAndStatus, filename = output)
+
+#as linegraph, 
+LinksByYearAndStatus_Line <- 
+  ggplot(
+    data = unique(all_with_meta), 
+    mapping = aes(x = year.published, color = binary_status)) + 
+  geom_line(stat = "count", size = 1) +
+  theme(axis.text.x = element_text(angle = 75, vjust = 1, hjust=1)) +
+  labs( y = "Number of Manuscripts Containing Links", 
+        x = "Year Published",
+        title = stringr::str_glue("Number of External User-Added Links by Year and Status (N={sum})"), 
+        color = "Link Status") +
+scale_color_manual(values = c("seagreen2", "indianred1"))
+LinksByYearAndStatus_Line
