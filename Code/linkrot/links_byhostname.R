43bf29b29ff9cd23435623beb49561cba1537155
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-07-30 16:01:13 -0400

    remove non-unique link graph from file, get count data

--- Code/linkrot/links_byhostname.R
+++ Code/linkrot/links_byhostname.R
@@ -18,48 +18,47 @@ unique_output <- input[3]
 #alllinks <- read_csv("Data/linkrot/groundtruth.alllinks.csv.gz")
 #metadatalinks <- read_csv("Data/linkrot/groundtruth.linksmetadata.csv.gz")
 
+# 20240730 - we only care about unique links 
 
 #group links by link_status
-all_type_tally <- alllinks %>%
-                    group_by(hostname) %>% 
-                    tally()
 unique_type_tally <- unique(alllinks) %>% 
                       group_by(hostname) %>%
                       tally()
-all_sum <- as.numeric(sum(all_type_tally$n)) 
 unique_sum <- as.numeric(sum(unique_type_tally$n))
 
 
+
+#20240730 - change to geom_point plot with percent alive/dead
+# include N on the y axis 
+
+distinct <- distinct(alllinks)
 #plot status of "more permanent hostname links" 
-long_lasting <- filter(alllinks, 
-                       grepl("doi|git|figshare|datadryad|zenodo|asm", hostname))
+long_lasting <- filter(distinct, 
+                       grepl("doi|git|figshare|datadryad|zenodo|asm", hostname)) 
 
-all_long_lasting_status <- 
-  ggplot(
-    data = long_lasting, 
-    mapping = aes(y = hostname, fill = binary_status)
-  ) + 
-  geom_bar(stat = "count") +
-  labs( x = "Number of Links", 
-        y = "Website Hostname",
-        title = stringr::str_glue("Total Number of External User-Added Links\nby Hostname and Status (N={all_sum})"), 
-        fill = "Link Status") +
-scale_fill_manual(values = c("seagreen2", "indianred1"))
-all_long_lasting_status
+#get count data 
+long_lasting <-
+  long_lasting %>% 
+    mutate(.by = paper, 
+          n_links = n(), 
+          n_dead = sum(!is_alive),
+          dead_fract = ((n_dead) / n_links), 
+          ) %>%
+    select(paper, n_links, n_dead, dead_fract)
 
-ggsave(all_long_lasting_status, filename = all_output)
+
 
 unique_long_lasting_status <- 
    ggplot(
-    data = unique(long_lasting), 
-    mapping = aes(y = hostname, fill = binary_status)
+    data = long_lasting, 
+    mapping = aes(y = hostname, color = binary_status)
   ) + 
-  geom_bar(stat = "count") +
+  geom_point(stat = "count") +
   labs( x = "Number of Links", 
         y = "Website Hostname",
-        title = stringr::str_glue("Total Number of External User-Added Links\nby Hostname and Status (N={all_sum})"), 
+        title = stringr::str_glue("Total Number of External User-Added Links\nby Hostname and Status (N={unique_sum})"), 
         fill = "Link Status") +
-scale_fill_manual(values = c("seagreen2", "indianred1"))
+scale_color_manual(values = c("seagreen2", "indianred1"))
 
 unique_long_lasting_status
 
