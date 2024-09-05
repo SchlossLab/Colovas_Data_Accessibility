9cc482122e8c24feb88c8e8e960aad49436e13df
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-09-05 16:52:41 -0400

    i don't know why this doesn't ever work

--- Code/doi_gathering_rscopus.R
+++ Code/doi_gathering_rscopus.R
@@ -7,35 +7,41 @@ library(tidyverse)
 library(rscopus)
 
 personal_key <- "f24a07ed9eea613f729d6469a816966c"
-institutional_key <- "7c25e0e82b37408e45c8da604e824725"
+institutional_token <- "7c25e0e82b37408e45c8da604e824725"
 
 set_api_key(personal_key)
 have_api_key()
 
-inst_token_header(institutional_key)
+hdr <- inst_token_header(institutional_token)
+
 get_api_key()
+elsevier_authenticate(headers = hdr)
+
+# token is from Scopus dev
+
+res <- author_df(last_name = "Muschelli", first_name = "John", verbose = FALSE, general = FALSE, headers = hdr)
 
 if (have_api_key()) {
-auth <- elsevier_authenticate(institutional_key, verbose = TRUE)
+auth <- elsevier_authenticate(personal_key, verbose = TRUE, headers = hdr)
 }
 
 rscopus::is_elsevier_guest()
 
-#example code from rscopus pkg
+#example code from rscopus 
 
-if (rscopus::is_elsevier_authorized()) {
-  res = author_df(last_name = "Muschelli", first_name = "John", verbose = FALSE, general = FALSE)
-  names(res)
-  head(res[, c("title", "journal", "description")])
-  unique(res$au_id)
-  unique(as.character(res$affilname_1))
-  
-  all_dat = author_data(last_name = "Muschelli", 
-                        first_name = "John", verbose = FALSE, general = TRUE)
-  res2 = all_dat$df
-  res2 = res2 %>% 
-    rename(journal = `prism:publicationName`,
-           title = `dc:title`,
-           description = `dc:description`)
-  head(res[, c("title", "journal", "description")])
-}
+# 20240905
+# with error message stored in result 
+#  "Requestor configuration settings insufficient for access to this resource."
+# bruh
+ if (!is.null(personal_key) & nchar(personal_key) > 0){
+       result <- citation_retrieval(pii = c("S0140673616324102",
+       "S0014579301033130"),
+       verbose = FALSE)
+       if (httr::status_code(result$get_statement) < 400) {
+          res <- parse_citation_retrieval(result)
+       }
+    }
+
+    
+    set_api_key(NULL)
+
