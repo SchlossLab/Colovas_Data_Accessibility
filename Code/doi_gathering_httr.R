553bf8870a4196890ffb212f81a78a762f978fae
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-09-12 16:37:41 -0400

    working on api test for scopus with httr2, worksgit add . !

--- Code/doi_gathering_httr.R
+++ Code/doi_gathering_httr.R
@@ -47,10 +47,20 @@ response <- req_perform(crossref_req) %>%
 scopus_key <- "f24a07ed9eea613f729d6469a816966c"
 scopus_institutional_token <- "7c25e0e82b37408e45c8da604e824725"
 
-scopus_req <- request("https://api.elsevier.com/content/abstract/citation-count") %>%
+# 20240912 - this works but i need to figure out what params to add to get what i need
+scopus_req <- request("https://api.elsevier.com/content/abstract/citation-count?scopus_id=33646008552") %>%
+    req_headers("X-ELS-APIKey" = scopus_key) %>%
+    req_headers("X-ELS-Insttoken" = scopus_institutional_token)
+    
+
+
+#playing with the request to try and get an issn to work 
+scopus_req <- request("http://api.elsevier.com/content/search/scopus?query=issn(1098-6596)&date(2000-2024)&cursor/@next&start=199&count=200&field=citedby-count,prism:doi,date") %>%
     req_headers("X-ELS-APIKey" = scopus_key) %>%
     req_headers("X-ELS-Insttoken" = scopus_institutional_token)
 
-
 scopus_response <- req_perform(scopus_req) %>%
     resp_body_json()
+
+# curl -v -H "X-ELS-APIKey:'f24a07ed9eea613f729d6469a816966c', X-ELS-Insttoken:'7c25e0e82b37408e45c8da604e824725'" 
+# "https://api.elsevier.com/content/abstract/citation-count?scopus_id=33646008552"
