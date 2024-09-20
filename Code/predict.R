6e5a54d5b4bed168e1d354e2175bde69b673e4b0
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-09-20 14:53:10 -0400

    added some comments in predict.r

--- Code/predict.R
+++ Code/predict.R
@@ -24,12 +24,15 @@ jb_small_tokens <- read_csv("Data/1098-5530_small.tokens.csv.gz")
 
 View(head(jb_small_tokens, 20))
 
+#the dataset isn't in the right format to be fed into the model to be predicted
+#need to use ML prep as a guide for how to deploy the models
+
 predictions <- predict(da_model, newdata = jb_small_tokens, type = 'prob')
+
 
 
 #from kelly
 # and then use that to predict the classification for the held out data.
 #predictions <- predict(final_model, newdata = test_data, type = 'prob')
 
-predictions <- predict(final_model, newdata = test_data, type = 'prob')
 
