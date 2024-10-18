4ce7e83f7d2e4c3114df6ec6acbc3fc1bea66bf4
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-10-18 17:04:32 -0400

    finally think i applied zscores correctly!

--- Code/ml_prep_predict.R
+++ Code/ml_prep_predict.R
@@ -23,19 +23,19 @@ ztable <- read_csv(input[4])
 token_groups <- readRDS(input[5])
 container_titles <-readRDS(input[6])
 
-# #local implementation
-clean_text <- read_csv("Data/1935-7885_alive.tokens.csv.gz")
-metadata <- read_csv("Data/1935-7885_alive.csv")
-# ml_var_snake <- "availability"
-ml_var <- c("paper", "container.title")
-output_file <- "Data/1935-7885_alive.preprocessed.RDS"
-#do i have a practice one yet?
-ztable_filename <- "Data/groundtruth.data_availability.zscoretable_filtered.csv"
-token_filename <- "Data/groundtruth.data_availability.tokenlist.RDS"
-ztable <- read_csv(ztable_filename)
-token_list <- readRDS(token_filename)
-container_titles <-
-    readRDS("Data/groundtruth.data_availability.container_titles.RDS")
+# # #local implementation
+# clean_text <- read_csv("Data/1935-7885_alive.tokens.csv.gz")
+# metadata <- read_csv("Data/1935-7885_alive.csv")
+# # ml_var_snake <- "availability"
+# ml_var <- c("paper", "container.title")
+# output_file <- "Data/1935-7885_alive.preprocessed.RDS"
+# #do i have a practice one yet?
+# ztable_filename <- "Data/groundtruth.data_availability.zscoretable_filtered.csv"
+# token_filename <- "Data/groundtruth.data_availability.tokenlist.RDS"
+# ztable <- read_csv(ztable_filename)
+# token_list <- readRDS(token_filename)
+# container_titles <-
+#     readRDS("Data/groundtruth.data_availability.container_titles.RDS")
 
 
 # set up the format of the clean_text dataframe 
@@ -62,18 +62,25 @@ clean_tibble <-
 need_meta <- select(metadata, all_of(ml_var))
 
 # join clean_tibble and need_meta 
-full_ml <- left_join(need_meta, clean_tibble, by = join_by(paper == paper_doi))
+full_ml <- left_join(need_meta, clean_tibble, by = join_by(paper == paper_doi)) %>%
+            rename(paper_doi = paper)
 
 
-#save full ml for troubleshooting purposes
-#saveRDS(full_ml, file = "Data/JMBE_full_ml.RDS")
-full_ml <- readRDS("Data/JMBE_full_ml.RDS")
+#sanity check - make sure paper_doi is first column 
+head(full_ml)
+
+
+# #save full ml for troubleshooting purposes
+# #saveRDS(full_ml, file = "Data/JMBE_full_ml.RDS")
+# full_ml <- readRDS("Data/JMBE_full_ml.RDS") %>%
+#     rename(paper_doi = paper)
+
 
 
 #pivot full_ml to long------------------------------------ 
 long_full_ml <-
     full_ml %>%
-        pivot_longer(cols = -c(paper, container.title), 
+        pivot_longer(cols = -c(paper_doi, container.title), 
                     names_to = "tokens", 
                     values_to = "num_appearances")
 #sanity check         
@@ -91,31 +98,31 @@ head(missing_full_ml_tokens, 20)
 nrow(missing_full_ml_tokens)
 
 
-#add missing columns to full_ml, but this isn't working
-full_ml_with_missing <-full_ml
+#add missing columns to full_ml
+full_ml_with_missing <- full_ml
 for (i in 1:nrow(missing_full_ml_tokens)) {
 
     missing_var <- missing_full_ml_tokens$tokens[[i]]
+    
     full_ml_with_missing <-
         full_ml_with_missing %>%
             mutate("{missing_var}" := 0)
 
 }
 
-#sanity check
-head(full_ml_with_missing, 20)
-nrow(full_ml)
-nrow(full_ml_with_missing)
-nrow(ztable)
-
+#sanity check, more columns 
+ncol(full_ml)
+ncol(full_ml_with_missing)
+head(full_ml_with_missing)
 
 #make full_ml_with_missing long again-------------------------------- 
 long_full_ml_with_missing <-
     full_ml_with_missing %>%
-        pivot_longer(cols = -c(paper, container.title), 
-                    names_to = "tokens", 
+        pivot_longer(cols = -c(paper_doi, container.title),
+                    names_to = "tokens",
                     values_to = "num_appearances")
 
+head(long_full_ml_with_missing, 20)
 
 #join with z table
 joined_full_ml_tokens <-semi_join(long_full_ml_with_missing, 
@@ -127,7 +134,7 @@ head(joined_full_ml_tokens)
 # pivot back to wide -------------------------------------------------
 wide_joined_full_ml_tokens <-
     joined_full_ml_tokens %>%
-        pivot_wider(id_cols = c(paper, container.title), 
+        pivot_wider(id_cols = c(paper_doi, container.title), 
                     id_expand = TRUE,
                     names_from = tokens, 
                     values_from = num_appearances, 
@@ -136,10 +143,10 @@ wide_joined_full_ml_tokens <-
 
 
 # create dummy variables for each of the journals---------------------
-# 20241018 - do i still need this in this file?
-# container_titles <-
-#     container_titles %>% 
-#         mutate(var_name = paste0("container.title_", container.title)) 
+
+container_titles <-
+    container_titles %>% 
+        mutate(var_name = paste0("container.title_", container.title)) 
 
     for (i in 1:nrow(container_titles)) {
     new_var <- container_titles$var_name[i]
@@ -149,6 +156,7 @@ wide_joined_full_ml_tokens <-
         mutate(
         "{new_var}" := ifelse(container.title == container_titles$container.title[i], 1, 0))
     }
+
 
 # remove container.title 
 wide_joined_full_ml_tokens <-
@@ -162,56 +170,66 @@ grep("container*", pivoted_colnames, value = TRUE)
 
 #collapse correlated features from training datasets------------------------------
 
+#i shouldn't need to do this anymore because i can just join with ztable 
 # iterate through each token group
 
-    keep_groups <- vector("character", length(token_list))
-    for(j in 1:length(token_list)){
-        #if none of the tokens are found in dataset
-            if(!any(token_list[[j]] %in% colnames(wide_joined_full_ml_tokens))) {
-            #add grp'i' column to dataset and fill with 0s
-            new_var <- paste0("grp", j)
-            wide_joined_full_ml_tokens <-
-                wide_joined_full_ml_tokens %>%
-                    mutate("{new_var}" := 0)
-            }
+#     keep_groups <- vector("character", length(token_list))
+#     for(j in 1:length(token_list)){
+#         #if none of the tokens are found in dataset
+#             if(!any(token_list[[j]] %in% colnames(wide_joined_full_ml_tokens))) {
+#             #add grp'i' column to dataset and fill with 0s
+#             new_var <- paste0("grp", j)
+#             wide_joined_full_ml_tokens <-
+#                 wide_joined_full_ml_tokens %>%
+#                     mutate("{new_var}" := 0)
+#             }
             
-            else {
-                new_var <- paste0("grp", j)
-                #get positions of true tokens in token list j
-                position <- which(any(token_list[[j]] %in% colnames(wide_joined_full_ml_tokens)))[1]
-                # give you column name
-                representative <- token_list[[j]][position]
-                wide_joined_full_ml_tokens <-
-                    wide_joined_full_ml_tokens %>%
-                    mutate("{new_var}" := wide_joined_full_ml_tokens$representative)
-            }
-    }
+#             else {
+#                 new_var <- paste0("grp", j)
+#                 #get positions of true tokens in token list j
+#                 position <- which(any(token_list[[j]] %in% colnames(wide_joined_full_ml_tokens)))[1]
+#                 # give you column name
+#                 representative <- token_list[[j]][position]
+#                 wide_joined_full_ml_tokens <-
+#                     wide_joined_full_ml_tokens %>%
+#                     mutate("{new_var}" := wide_joined_full_ml_tokens$representative)
+#             }
+#     }
 
-#sanity check 
-pivoted_colnames2 <- colnames(wide_joined_full_ml_tokens)
-grep("grp", pivoted_colnames2, value = TRUE) 
+# #sanity check 
+# pivoted_colnames2 <- colnames(wide_joined_full_ml_tokens)
+# grep("grp", pivoted_colnames2, value = TRUE) 
 
 
 #finally apply z score to the data!--------------------------
 
-# 20241017
-# i have no clue what just happened because apparently now none 
-# of the colnames are in the ztable?????
-# which i swear i added them all 
+#sanity check that all colnames are in ztable 
+#and all ztable are in the colnames 
+#except for paper_doi of course
 wide_colnames <- colnames(wide_joined_full_ml_tokens)
-wide_colnames == ztable_full$tokens
+ztable$tokens %in% wide_colnames
+wide_colnames %in% ztable$tokens
 
-ztable_full$tokens %in% wide_colnames
+#check for NAs
+any(is.na.data.frame(wide_joined_full_ml_tokens))
+
+ncol(wide_joined_full_ml_tokens)
+nrow(ztable)
 
 
-for(i in 1:ncol(wide_joined_full_ml_tokens)){
-wide_joined_full_ml_tokens[ztable_full[[1]][i]] %>%
-    map(., \(x) ((x-ztable_full[[2]][i])/ztable_full[[3]][i]))
+#apply z scoring!!!
+
+for(i in 1:nrow(ztable)){
+    zscored_table[ztable[[1]][[i]]] <-
+    wide_joined_full_ml_tokens[ztable[[1]][[i]]] %>%
+        modify(., \(x) ((x-ztable[[2]][[i]])/ztable[[3]][[i]]))
 }
 
+wide_joined_full_ml_tokens[ztable[[1]][[1]]]
+head(zscored_table, 20)
 
 
 
 # eventually - save preprocessed data as an RDS file 
-saveRDS(full_ml_pre_prediction, file = output_file)
+saveRDS(zscored_table, file = output_file)
 
