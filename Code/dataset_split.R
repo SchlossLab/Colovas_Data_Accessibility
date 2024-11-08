d12f397d28bbcadae4706b428ab0b42f072787db
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-11-08 16:08:37 -0500

    after issues with splitting 3 ways, just split big data exactly in half

--- Code/dataset_split.R
+++ Code/dataset_split.R
@@ -17,12 +17,16 @@ csv <- input[1]
 data_processed <- read_csv(csv)
 filepath <- input[2]
 
-# #local practice 
+set.seed(19991028)
+# # #local practice 
 # csv <- "Data/papers/0095-1137.csv"
 # data_processed <- read_csv(csv)
+# nrow(data_processed)
 
-data_1 <- slice_head(data_processed, n = 10000)
+data_1 <- slice_head(data_processed, prop = 1/2)
+nrow(data_1)
 data_2 <- anti_join(data_processed, data_1)
+nrow(data_2)
 
 write_csv(data_1, file = paste0(filepath, "-1.csv"))
 write_csv(data_2, file = paste0(filepath, "-2.csv"))
