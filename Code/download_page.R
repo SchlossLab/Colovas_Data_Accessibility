409ae598dd55bc58e9cc22112dc9d68962c2465c
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2024-11-12 15:51:14 -0500

    got page downloading working!

--- Code/download_page.R
+++ Code/download_page.R
@@ -22,8 +22,8 @@ input_file <- read_csv("Data/doi_linkrot/alive/1935-7885.csv")
 # colnames(input_file)
 output <- "Data/html/1935-7885/"
 
-# okay so we're going to do the validity check and scrape in the same thing? 
 
+## 20241112 - took this from doi_linkrot 
 # get_site_status_no_follow <- function(websiteurl) {
   
 #   response <- tryCatch( {request(websiteurl) %>% 
@@ -37,32 +37,35 @@ output <- "Data/html/1935-7885/"
   
 # }
 
+# add unique_id to table to tell you what the filename will be
 input_file <- input_file %>% 
     mutate(unique_id = str_split_i(input_file$doi, "/", 2))
 
+# start loop to go through each paper and save output
+input_file_small <- 
+  slice_head(input_file, n = 20) %>%
+  mutate(link_status = as.numeric(0)) 
+
+str(input_file_small$link_status)
 
 
-one_paper <- input_file$paper[1]
-one_doi <- input_file$doi[1]
+download_html <- function(input_file) {
+  for(i in 1:nrow(input_file)){
+    response <- tryCatch( {request(input_file$paper[i]) %>% 
+        req_options(followlocation = FALSE) %>%
+        req_error(is_error = ~ FALSE) %>% 
+        req_perform()}, error = \(x){list(status_code = 404) } )
 
-html <- read_html(one_paper)
+      input_file$link_status[i] <- as.numeric(response$status_code)
+      html <- response %>% resp_body_html()
+      filename <- paste0(output, input_file_small$unique_id[i], ".html")
 
-#use save_html in htmltools
+      write_html(html, file = filename)
+  }
+}
 
-#have to figure out how to save these, might need just doi or put doi in quotes
-htmltools::save_html(html, file = paste0(output, unique_id))
-
-
-#20241112 - try getting status and html in one hit 
-#only will work if httr2 is activated
-
-response <- tryCatch( {request(one_paper) %>% 
-    req_options(followlocation = FALSE) %>%
-    req_error(is_error = ~ FALSE) %>% 
-    req_perform()}, error = \(x){list(status_code = 404) } )
+#~14 seconds for 20 files 
+download_html(input_file_small)
 
 
-  numeric_response <- response$status_code
-  html <- response %>% resp_body_html()
-
-# what do i return to let snakemake know i'm done? 
+# see pat note and ask greg for help with snakemake rules 
