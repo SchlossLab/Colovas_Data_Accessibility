c39ea526c704fa2e1d453c773f23d6d102dc658a
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-01-09 11:37:54 -0500

    starting the prelim figures, working on what metrics and how to show

--- Code/da_prelim_figs.R
+++ Code/da_prelim_figs.R
@@ -7,42 +7,91 @@
 library(tidyverse)
 
 #import data 
-predicted_files <-read_csv("Data/final/predicted_results.csv.gz")
-head(predictions)
+# predicted_files <-read_csv("Data/final/predicted_results.csv.gz")
+# head(predictions)
 
-lookup_table <-read_csv("Data/papers/lookup_table.csv.gz")
-head(lookup_table)
+# lookup_table <-read_csv("Data/papers/lookup_table.csv.gz")
+# head(lookup_table)
 
-joined_predictions <- full_join(predicted_files, lookup_table, by = join_by("file" == "html_filename")) 
-head(joined_predictions)
+# joined_predictions <- full_join(predicted_files, lookup_table, by = join_by("file" == "html_filename")) 
+# head(joined_predictions)
 
-papers_dir <- "Data/papers"
-csv_files <- list.files(papers_dir, "*.csv", full.names = TRUE) 
+# papers_dir <- "Data/papers"
+# csv_files <- list.files(papers_dir, "*.csv", full.names = TRUE) 
 
-keep_track<-tibble()
-for(i in 1:12) {
-    csv_file <- read_csv(csv_files[i])
-    #this does it for the current journal to join with all_papers
-    all_papers <- left_join(csv_file, joined_predictions) %>%
-        mutate_if(is.double, as.character, .vars = "issue") %>%
-        mutate_if(lubridate::is.Date, as.character, .vars = "created")
-    keep_track<-bind_rows(keep_track, all_papers)
+# keep_track<-tibble()
+# for(i in 1:12) {
+#     csv_file <- read_csv(csv_files[i])
+#     #this does it for the current journal to join with all_papers
+#     all_papers <- left_join(csv_file, joined_predictions) %>%
+#         mutate_if(is.double, as.character, .vars = "issue") %>%
+#         mutate_if(lubridate::is.Date, as.character, .vars = "created")
+#     keep_track<-bind_rows(keep_track, all_papers)
    
-}
+# }
 
-# 20250107 - keep_track contains all metadata and all predictions and filenames
+# write_csv(keep_track, file = "Data/final/predictions_with_metadata.csv.gz")
+
+metadata <- read_csv("Data/final/predictions_with_metadata.csv.gz")
 
 #now we can start doing the fun graphing part
 
 # make column for date published (issued) 
-metadata <- keep_track %>% 
+metadata <- metadata %>% 
     mutate(year.published = str_sub(issued, start = 1, end = 4))
-metadata %>% count(year.published)
 
 
-#let's graph nsd over time 
-#this doesn't work but honestly what does in my life let's be real 
 
-ggplot(data = metadata, 
-    mapping = aes(x = nsd, .by = year.published)) + 
-    geom_bar(stat = "count")
+
+
+
+#number of papers over time
+
+metadata %>% 
+    count(year.published) %>% 
+    ggplot(mapping = aes(y = `n`, 
+                    x = year.published)) + 
+    geom_point() + 
+    labs(x = "Year Published", 
+        y = "Number of Papers", 
+        title = "Number of Papers Published Each Year in ASM Journals 2000-2024")
+
+
+#number of nsd papers over time 
+metadata %>% 
+    count(year.published, nsd) %>% 
+    ggplot(mapping = aes(y = `n`, 
+                    x = as.numeric(year.published), 
+                    color = nsd)) + 
+    geom_line(stat = "identity") + 
+    labs(x = "Year Published", 
+        y = "Number of Papers",
+        color = "New Sequencing \nData Available",
+        title = "Number of Papers Published Each Year Containing New Sequeunce Data in ASM Journals 2000-2024")
+
+#number of da papers over time 
+metadata %>% 
+    count(year.published, da) %>% 
+    ggplot(mapping = aes(y = `n`, 
+                    x = as.numeric(year.published), 
+                    color = da)) + 
+    geom_line(stat = "identity") + 
+    labs(x = "Year Published", 
+        y = "Number of Papers",
+        color = "Was Data Available?",
+        title = "Number of Papers Published Each Year Containing Available Data in ASM Journals 2000-2024")
+
+
+#number of da and nsd papers over time 
+#idk what the best way to graph this is
+metadata %>% 
+    count(year.published, da, nsd) %>% 
+    ggplot(mapping = aes(y = `n`, 
+                    x = as.numeric(year.published),
+                    fill = da)) + 
+    geom_col() + 
+    labs(x = "Year Published", 
+        y = "Number of Papers",
+        color = "Was Data Available?",
+        title = "Number of Papers Published Each Year Containing Available Data in ASM Journals 2000-2024")
+
