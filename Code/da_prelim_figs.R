4d4fcb2b3f0622a4fd72d4c2310376096a9189aa
Joanna Colovas <joannacolovas@gmail.com>
2025-01-28 15:07:16 -0500

    add 0 intercept to modeling

--- Code/da_prelim_figs.R
+++ Code/da_prelim_figs.R
@@ -41,21 +41,22 @@ metadata <- metadata %>%
     mutate(year.published = str_sub(issued, start = 1, end = 4), 
             years.since.published = (2024-as.numeric(year.published)), 
              citations.per.year = (is.referenced.by.count/years.since.published), 
-             issued.date = ymd(issued, truncated = 2), 
-             months.since.y2k = interval(ymd("2000-01-01"), metadata$issued.date) %/% months(1), 
-            age.in.months = interval(metadata$issued.date, ymd("2025-01-01")) %/% months(1))
+             issued.date = ymd(issued, truncated = 2))
+metadata <- metadata %>% 
+  mutate(months.since.y2k = interval(ymd("2000-01-01"), metadata$issued.date) %/% months(1), 
+          age.in.months = interval(metadata$issued.date, ymd("2025-01-01")) %/% months(1))
 
 colnames(metadata)
 #modeling using time intervals
 summary(lm(is.referenced.by.count~da + nsd + months.since.y2k, data = metadata))
-summary(lm(is.referenced.by.count~da + nsd + age.in.months, data = metadata))
+summary(lm(is.referenced.by.count~ 0 + da + nsd + age.in.months, data = metadata))
 
 #modeling with nsd yes only data
 nsd_yes_metadata <- 
   metadata %>% 
     filter(nsd == "Yes")
 
-summary(lm(is.referenced.by.count~da + age.in.months, data = nsd_yes_metadata))
+summary(lm(is.referenced.by.count~0+da + age.in.months, data = nsd_yes_metadata))
 
 
 #number of papers over time
