61fa86dd8b80e7e7550fe406663691f209ffead0
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-01-31 16:41:13 -0500

    Trying to get wos to work by limiting number of cores it can use

--- Code/doi_gathering_wos_via_httr.R
+++ Code/doi_gathering_wos_via_httr.R
@@ -12,7 +12,7 @@ issn <- as.character(input[1])
 
 
 #local testing
-# issn <-"1935-7885"
+#issn <-"2379-5042"
 
 #   clarivate - 
 clarivate_key <- "fba06c10b8832254cfed5f514778b86e9f888e51"
@@ -47,7 +47,7 @@ request_url<-paste0(
 wos_req <- request(request_url) %>%
     req_headers("X-ApiKey" = clarivate_key) %>% 
     req_user_agent("jocolova@med.umich.edu") %>%
-    req_throttle(rate = 5/60)
+    req_throttle(rate = 1/60)
 
 wos_response <- req_perform(wos_req) %>%
     resp_body_json(simplifyVector = TRUE) 
@@ -61,7 +61,7 @@ page_results[[i]]<-
 
 all_results<-tibble(page_results, .name_repair = "minimal") %>% 
     unnest(cols = page_results) %>%
-    select(-authors, -pages, -anonymous, -authorKeywords)
+    select(-authors, -pages, -authorKeywords)
 
 write_csv(all_results, file = paste0("Data/wos/wos_", issn, ".csv.gz"))
 
