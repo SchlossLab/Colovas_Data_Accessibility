d8d1235909fe47d2e9a56761574cad4575dc3333
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-02-06 15:11:12 -0500

    save as two ncbi files

--- Code/scratch_open_edirect.R
+++ Code/scratch_open_edirect.R
@@ -7,15 +7,23 @@ library(tidyverse)
 library(xml2)
 
 
-#filename 
-filename <- "1098-5522_efetch.csv"
+#create all_ncbi files 
+filenames<-list.files("Data/ncbi", full.names = TRUE)
+
 columns <- c("UID", "title", "authors", "citation_info", "first_author", "journal_name",
  "pub_year", "pub_date", "NA", "NA", "doi")
 
-file_import <-read_csv(filename, col_names = columns )
+all_ncbi <-read_csv(filenames, col_names = columns)
 
-file_import %>%  
-    filter(pub_year >=2000 & pub_year <= 2024) %>% 
-    count(pub_year)
+ncbi_eliminated <- all_ncbi %>%  
+        filter(pub_year < 2000 | pub_year > 2024) 
+       
+ncbi_eliminated %>% count(pub_year) %>% print(n = Inf)
 
-view(head(file_import))
+ncbi_25 <-
+    all_ncbi %>%  
+        filter(pub_year >=2000 & pub_year <= 2024) 
+
+write_csv(ncbi_25, file = "Data/ncbi/ncbi_all_papers.csv.gz")
+write_csv(ncbi_eliminated, file = "Data/ncbi/ncbi_eliminated_papers.csv.gz")
+
