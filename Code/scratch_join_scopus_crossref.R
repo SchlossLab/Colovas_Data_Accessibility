cc49cf8572a7731cf632c8ffdd83d983f6bdd58a
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-02-06 15:46:51 -0500

    figure out how many records are in common between the databases

--- Code/scratch_join_scopus_crossref.R
+++ Code/scratch_join_scopus_crossref.R
@@ -8,13 +8,17 @@ library(tidyverse)
 scopus <-read_csv("Data/scopus/all_scopus_citations.csv.gz")
 
 crossref <-read_csv("Data/papers/all_papers.csv.gz") 
+
+ncbi <-read_csv("Data/ncbi/ncbi_all_papers.csv.gz")
+
 crossref <-
     crossref %>% 
         mutate(doi_no_slash = str_replace(doi, "_", "/"))
 head(scopus)
 head(crossref)
+colnames(ncbi)
 
-
+#from when it was just scopus and crossref
 both_only_scopus<-inner_join(scopus, crossref, by = join_by(`prism:doi` == doi_no_slash))
 both_only_crossref<-inner_join(crossref, scopus, by = join_by(doi_no_slash == `prism:doi`))
 scopus_only<-anti_join(scopus, crossref, by = join_by(`prism:doi` == doi_no_slash))
@@ -28,3 +32,31 @@ view(scopus_only)
 # scopus only 93K
 # both 18K
 # why can't i get the pubyear or date from scopus idk 
+
+unique(ncbi)
+unique(scopus)
+unique(crossref)
+
+all_three<-inner_join(scopus, crossref, by = join_by(`prism:doi` == doi_no_slash)) %>%
+    inner_join(., ncbi, by = join_by(`prism:doi` == doi))
+
+scopus_and_crossref<-inner_join(scopus, crossref, by = join_by(`prism:doi` == doi_no_slash))
+crossref_and_scopus<-inner_join(crossref, scopus, by = join_by(doi_no_slash == `prism:doi`))
+
+scopus_and_ncbi<-inner_join(scopus, ncbi, by = join_by(`prism:doi` == doi), relationship = "many-to-many")
+crossref_and_ncbi<-inner_join(crossref, ncbi, by = join_by(doi_no_slash == doi))
+
+
+
+scopus_only<-anti_join(scopus, crossref, by = join_by(`prism:doi` == doi_no_slash)) %>%
+   anti_join(., ncbi, by = join_by(`prism:doi` == doi))
+
+crossref_only<-anti_join(crossref, scopus, by = join_by(doi_no_slash == `prism:doi`)) %>%
+   anti_join(., ncbi, by = join_by(doi_no_slash == doi))
+
+ncbi_only <- anti_join(ncbi, scopus, by = join_by(doi == `prism:doi`)) %>%
+   anti_join(., crossref, by = join_by(doi == doi_no_slash))
+
+ncbi
+crossref
+scopus
