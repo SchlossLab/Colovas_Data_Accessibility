86883a67864d3d8f11be36e0d890a698aabd4099
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-05-29 16:55:45 -0400

    cluster is being janky but working on figures

--- Code/negative_binomial.R
+++ Code/negative_binomial.R
@@ -8,6 +8,7 @@ library(emmeans)
 library(jtools)
 
 
+
 #load metadata
 metadata <- read_csv("Data/final/predictions_with_metadata.csv.gz")
 
@@ -31,16 +32,16 @@ nsd_yes_metadata <-
     filter(nsd == "Yes") %>%
     mutate(da_factor = factor(da)) 
 
-
+#318-320
 interaction <-glm.nb(is.referenced.by.count~ da_factor + age.in.months + da_factor * age.in.months, data = nsd_yes_metadata, link = log)
-no_interaction <-glm.nb(is.referenced.by.count~ da_factor + age.in.months, data = nsd_yes_metadata, link = log)
-three_terms_no_int <-glm.nb(is.referenced.by.count~ da_factor + age.in.months + container.title, data = nsd_yes_metadata, link = log)
-three_terms_int <-glm.nb(is.referenced.by.count~ da_factor + age.in.months + container.title + da_factor*container.title, data = nsd_yes_metadata, link = log)
+# no_interaction <-glm.nb(is.referenced.by.count~ da_factor + age.in.months, data = nsd_yes_metadata, link = log)
+# three_terms_no_int <-glm.nb(is.referenced.by.count~ da_factor + age.in.months + container.title, data = nsd_yes_metadata, link = log)
+# three_terms_int <-glm.nb(is.referenced.by.count~ da_factor + age.in.months + container.title + da_factor*container.title, data = nsd_yes_metadata, link = log)
 three_terms_int_all <-glm.nb(is.referenced.by.count~ da_factor + log(age.in.months) + container.title + log(age.in.months)*da_factor*container.title, data = nsd_yes_metadata, link = log)
 jtools::summ(interaction)
-jtools::summ(no_interaction)
-jtools::summ(three_terms_no_int)
-jtools::summ(three_terms_int)
+# jtools::summ(no_interaction)
+# jtools::summ(three_terms_no_int)
+# jtools::summ(three_terms_int)
 jtools::summ(three_terms_int_all)
 
 #i actually want to see if i filter these better if the models make more sense 
@@ -59,6 +60,34 @@ iai_interaction <-glm.nb(is.referenced.by.count~ da_factor + age.in.months, data
 iai_interaction_2 <-glm.nb(is.referenced.by.count~ da_factor + log(age.in.months) + da_factor*log(age.in.months), data = iai, link = log)
 jtools::summ(iai_interaction_2)
 
+#graphing with jtools 
+
+plot_1<-jtools::effect_plot(interaction, da_factor, plot.points = TRUE)
+jtools::effect_plot(interaction, age.in.months, plot.points = TRUE)
+plot_2<-effect_plot(three_terms_int_all, age.in.months, interval = TRUE, int.type = "confidence", 
+                    int.width = 0.8, plot.points = TRUE, data = nsd_yes_metadata, line.colors = "blue", 
+                    outcome.scale = "link")
+
+plot_2_no_points<-effect_plot(three_terms_int_all, age.in.months, interval = TRUE, int.type = "confidence", 
+                    int.width = 0.8, data = nsd_yes_metadata, line.colors = "blue", 
+                    outcome.scale = "link")
+
+plot_3<-effect_plot(three_terms_int_all, da_factor, interval = TRUE, int.type = "confidence", 
+                    int.width = 0.8, plot.points = TRUE, data = nsd_yes_metadata, line.colors = "blue", 
+                    outcome.scale = "link")
+ggsave(plot_2, file = "Figures/test_2.png")
+ggsave(plot_2_no_points, file = "Figures/test_nopoints_2.png")
+ggsave(plot_3, file = "Figures/test_3.png")
+
+
+
+
+
+
+
+
+
+#emmeans stuff but idk how much i like this 
 EMM <-emmeans(interaction, ~ da_factor * age.in.months)
 
 con<-pairs(EMM, simple = "da_factor")
