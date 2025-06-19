5e82d74c1f29fbc1738ca525b831e268ff042964
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-06-19 17:11:26 -0400

    working on fixing linkrot-needed loop

--- Code/non_snakemake/linkrot_troubleshoot.R
+++ Code/non_snakemake/linkrot_troubleshoot.R
@@ -55,21 +55,31 @@ conda_test <-c(link_status = get_site_status("https://anaconda.org/search?q=http
                 no_follow = get_site_status_no_follow("https://anaconda.org/search?q=httpgd"))
 conda_test
 
-githubt_test <-c(link_status = get_site_status("https://github.com/tidyverse/tidyverse"), 
+github_test <-c(link_status = get_site_status("https://github.com/tidyverse/tidyverse"), 
                 no_follow = get_site_status_no_follow("https://github.com/tidyverse/tidyverse"))
-githubt_test
+github_test
 
 
 #is it the mutate causing problems? 
 # or is it still iterating through the data appropriately?
-punctuation <-punctuation %>%
-    mutate(no_punctuation_status = get_site_status(no_punctuation), 
-        no_punctuation_no_follow_status = get_site_status_no_follow(no_punctuation),
-        link_status_no_follow = get_site_status_no_follow(link_address),
-        link_status_retry = get_site_status(link_address))
+# punctuation <-punctuation %>%
+#     mutate(no_punctuation_status = get_site_status(no_punctuation), 
+#         no_punctuation_no_follow_status = get_site_status_no_follow(no_punctuation),
+#         link_status_no_follow = get_site_status_no_follow(link_address),
+#         link_status_retry = get_site_status(link_address), 
+#         request = request(link_address))
+
+punctuation_map <-punctuation %>%
+    mutate(no_punctuation_status = map(no_punctuation, get_site_status), 
+        no_punctuation_no_follow_status = map(no_punctuation, get_site_status_no_follow),
+        link_status_no_follow = map(link_address, get_site_status_no_follow),
+        link_status_retry = map(link_address, get_site_status))
 
 #why does the "link_status_retry" differ from the original link status??
 #see line 98 in linkrot to compare original application of function get_site_status
-punctuation %>% count(link_status, no_punctuation_status, no_punctuation_no_follow_status, link_status_no_follow, link_status_retry) %>% 
-view()
+# punctuation %>% count(link_status, no_punctuation_status, no_punctuation_no_follow_status, link_status_no_follow, link_status_retry) 
+punctuation_map %>% count(link_status, no_punctuation_status, no_punctuation_no_follow_status, link_status_no_follow, link_status_retry) 
+
+# write_csv(punctuation, file = "Data/tests/linkrot/punctuation_GL_test.csv")
+write_csv(punctuation_map, file = "Data/tests/linkrot/punctuation_map_GL_test.csv")
 
