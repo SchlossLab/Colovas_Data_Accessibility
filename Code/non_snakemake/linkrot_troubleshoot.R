5adcd9ad9ce9ec0f6f463cde95bd32cf34b34e1f
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-06-24 11:10:30 -0400

    remove unneeded code to simplify file, fixed with pat, ready for cluster

--- Code/non_snakemake/linkrot_troubleshoot.R
+++ Code/non_snakemake/linkrot_troubleshoot.R
@@ -12,21 +12,21 @@ linkrot<-read_csv("Data/final/linkrot_combined.csv.gz")
 
 #12045 links ending with punctuation/ and not 200 status (link OK)
 punctuation <-linkrot %>%
-    filter(str_ends(link_address, "\\.")| str_ends(link_address, "\\,") | str_ends(link_address, "\\;") 
-    | str_ends(link_address, "\\:") | str_ends(link_address, "\\(") | str_ends(link_address, "\\)")) %>%
+    filter(str_ends(link_address, "\\.") | str_ends(link_address, "\\)")) %>% # remove only terminal . and )
+    filter(!str_detect(link_address, "\\(")) %>% #exclude anything with an open (
     filter(link_status != 200)
 
-#removing '| str_ends(link_address, "[:punct:]")' - removes about 12k from this search 
 
 #remove the final punctuation
 punctuation <-punctuation %>%
     mutate(no_punctuation = str_sub(link_address, start = 1, end = -2)) 
 
 #from linkrot - original function that follows links 
+# 20250624 - we don't really care in a functional sense if we're redirected
 get_site_status <- function(websiteurl) {
   
   response <- tryCatch( {request(websiteurl) %>% 
-      req_options(followlocation = TRUE) %>% # this line 'follows' links that redirect the user
+      req_options(followlocation = TRUE) %>% # this line 'follows' links that redirect the user, 
       req_error(is_error = ~ FALSE) %>% 
       req_retry(retry_on_failure = FALSE) %>% 
       req_perform()}, error = \(x){list(status_code = 404) } )
@@ -36,67 +36,12 @@ get_site_status <- function(websiteurl) {
   
 }
 
-#want to see if results change if we don't follow links
-get_site_status_no_follow <-function(websiteurl) {
-  
-  response <- tryCatch( {request(websiteurl) %>% 
-      req_options(followlocation = FALSE) %>%  #this line stops 'following' links that redirect the user
-      req_error(is_error = ~ FALSE) %>% 
-      req_perform()}, error = \(x){list(status_code = 404) } )
-  
-  numeric_response <- response$status_code
-  return(numeric_response)
-  
-}
 
-#test links we know work 
-umich_test <-c(link_status = get_site_status("umich.edu"), 
-                no_follow = get_site_status_no_follow("umich.edu"))
-umich_test
-
-conda_test <-c(link_status = get_site_status("https://anaconda.org/search?q=httpgd"), 
-                no_follow = get_site_status_no_follow("https://anaconda.org/search?q=httpgd"))
-conda_test
-
-github_test <-c(link_status = get_site_status("https://github.com/tidyverse/tidyverse"), 
-                no_follow = get_site_status_no_follow("https://github.com/tidyverse/tidyverse"))
-github_test
-
-
-#is it the mutate causing problems? 
-# or is it still iterating through the data appropriately?
-# punctuation <-punctuation %>%
-#     mutate(no_punctuation_status = get_site_status(no_punctuation), 
-#         no_punctuation_no_follow_status = get_site_status_no_follow(no_punctuation),
-#         link_status_no_follow = get_site_status_no_follow(link_address),
-#         link_status_retry = get_site_status(link_address), 
-#         request = request(link_address))
 
 punctuation_map <-punctuation %>%
-    mutate(no_punctuation_status = map_dbl(no_punctuation, get_site_status), 
-        no_punctuation_no_follow_status = map_dbl(no_punctuation, get_site_status_no_follow),
-        link_status_no_follow = map_dbl(link_address, get_site_status_no_follow),
-        link_status_retry = map_dbl(link_address, get_site_status))
+    mutate(no_punctuation_status = map_dbl(no_punctuation, get_site_status))
 
-#why does the "link_status_retry" differ from the original link status??
-#see line 98 in linkrot to compare original application of function get_site_status
-# punctuation %>% count(link_status, no_punctuation_status, no_punctuation_no_follow_status, link_status_no_follow, link_status_retry) 
-punctuation_map %>% count(link_status, no_punctuation_status, no_punctuation_no_follow_status, link_status_no_follow, link_status_retry) 
+punctuation_map %>% count(link_status, no_punctuation_status)
 
-# write_csv(punctuation, file = "Data/tests/linkrot/punctuation_GL_test.csv")
-write_csv(punctuation_map, file = "Data/tests/linkrot/punctuation_map_GL_test.csv")
+write_csv(punctuation_map, file = "Data/linkrot_datasets/no_punctuation_status.csv")
 
-
-#20260623 - make a smaller test set 
-
-# test_set <- punctuation[1:15,]
-
-# test_map_2 <-test_set %>%
-#     mutate(no_punctuation_status = map_dbl(no_punctuation, get_site_status))
-# #320 start
-
-# #3:00pm start finish - easily a minute per link 
-
-# test_map %>% count(no_punctuation_status)
-
-# test_map_2 %>% view()
