e1a27c9dc0d5400b93fd37d75d84cf400d473cca
Joanna Colovas <joannacolovas@gmail.com>
2025-07-08 17:03:32 -0400

    working on graphing practice

--- Code/dharma_test.R
+++ Code/dharma_test.R
@@ -11,14 +11,20 @@ library(DHARMa)
 
 
 #load metadata
-nsd_yes_metadata <- read_csv("Data/final/nsd_yes_metadata.csv.gz")
+# nsd_yes_metadata <- read_csv("Data/final/nsd_yes_metadata.csv.gz")
+#load local metadata
 
+nsd_yes_metadata <- read_csv("~/Documents/Schloss/Colovas_Data_Accessibility/Data/final/nsd_yes_metadata.csv.gz")
 
+nsd_yes_metadata <- nsd_yes_metadata %>%  
+  filter(., age.in.months != "NA") 
+
+#430 start- 430 finish damn 
 total_model <-glm.nb(is.referenced.by.count~ da_factor + log(age.in.months) + container.title + 
         + container.title*da_factor + log(age.in.months)*da_factor + container.title*log(age.in.months) + 
         log(age.in.months)*da_factor*container.title, data = nsd_yes_metadata, link = log)
 
-simulationOutput <- simulateResiduals(fittedModel = total_model, plot = F)
+simulationOutput <- simulateResiduals(fittedModel = total_model, plot = T)
 #how do i get the plot if the plot popups are blocked by using extra memory? 
 str(simulationOutput)
 
@@ -26,5 +32,9 @@ residuals(simulationOutput)
 
 plot <- plot(simulationOutput)
 
-plot(simulationOutput) %>% 
-ggsave(., file = "Figures/simulation_residuals.png")
+plotQQunif(simulationOutput)
+plotResiduals(simulationOutput)
+
+plotResiduals(simulationOutput, form = nsd_yes_metadata$age.in.months)
+plotResiduals(simulationOutput, form = nsd_yes_metadata$da_factor)
+plotResiduals(simulationOutput, form = nsd_yes_metadata$container.title) 
