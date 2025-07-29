1f259e308943b37d42fa4a2a9a103040132b3e97
Joanna Colovas <joannacolovas@gmail.com>
2025-07-29 16:05:16 -0400

    graphing glmer residuals

--- Code/tests/glmer_test.R
+++ Code/tests/glmer_test.R
@@ -5,16 +5,19 @@
 library(lme4)
 library(tidyverse)
 library(sjPlot)
+library(msm)
 
 
 #load local metadata
 nsd_yes_metadata <- read_csv("~/Documents/Schloss/Colovas_Data_Accessibility/Data/final/nsd_yes_metadata.csv.gz")
 
 #filter out NAs (important later)
-nsd_yes_metadata <- nsd_yes_metadata %>%
+nsd_yes_metadata <-
+  nsd_yes_metadata %>%
   filter(., age.in.months != "NA" & da_factor != "NA" & container.title != "NA") %>% 
   mutate(da_factor = factor(da_factor), 
-         container.title = factor(container.title))
+         container.title = factor(container.title)) %>% 
+  filter(container.title != "Journal of Microbiology &amp; Biology Education")
 
 # total_model <-glm.nb(is.referenced.by.count~ da_factor + log(age.in.months) + container.title + 
 #                        + container.title*da_factor + log(age.in.months)*da_factor + container.title*log(age.in.months) + 
@@ -24,29 +27,122 @@ nsd_yes_metadata <- nsd_yes_metadata %>%
 nsd_yes_10percent <- nsd_yes_metadata %>% 
   slice_sample(., prop = .1, weight_by = container.title)
 
-#start 3:12-325 = 10% of data 
+#start 11:24 = 10% of data 
 #relative formula = Y ~ data_avl + age + data_avl*age + (1 +data_avl + age + data_avl*age|journal) 
 ten_percent_glmer <- glmer.nb(formula = is.referenced.by.count ~ da_factor + age.in.months + da_factor*age.in.months + 
                                 (1+da_factor + age.in.months + da_factor*age.in.months|container.title), 
                               data = nsd_yes_10percent)
 
-
-  # plot_model <- plot_model(model, type = "pred", 
-      # terms = c("da_factor", "age.in.months[12,60,84,120,180]"))     
+#how do we plot the residuals for this??
+     
       
 plot(ten_percent_glmer)
 plot_model(ten_percent_glmer, type = "pred", terms = c("da_factor", "age.in.months[12,60,84]"), 
            bias_correction = FALSE)
+ggsave("~/Documents/Schloss/Colovas_Data_Accessibility/Figures/glmer_10_pred.png")
+residuals(ten_percent_glmer)
+
+# residuals <-
+  residuals(ten_percent_glmer) %>%  tibble(residual = .) %>%  
+  ggplot(aes(x = residual)) + 
+  geom_histogram(linewidth = 0.2, color = "white", bins = 100) +
+  ggtitle("Residuals Plotted for 10% of data glmer.nb") 
+  ggsave("~/Documents/Schloss/Colovas_Data_Accessibility/Figures/glmer_10_residuals.png")
+
+  
 
    
-#let's try the other formula 4:24 -not done at 4:49 done at 4:52
-#y ~ data_avl + age + data_avl*age + (1+data_avl|journal) +
-#(0+age|journal) + (0+data_avl*age|journal)
+# #let's try the other formula 4:24 -not done at 4:49 done at 4:52
+# #y ~ data_avl + age + data_avl*age + (1+data_avl|journal) +
+# #(0+age|journal) + (0+data_avl*age|journal)
+# 
+# ten_percent_glmer_2 <- glmer.nb(formula = is.referenced.by.count ~ da_factor + age.in.months + da_factor*age.in.months + 
+#                                 (1 + da_factor|container.title) + (0 +age.in.months|container.title)  +
+#                                 (0+ da_factor*age.in.months|container.title), 
+#                               data = nsd_yes_10percent)
+# 
+# plot_model(ten_percent_glmer_2, type = "pred", terms = c("da_factor", "age.in.months[12,60,84]"), 
+#            bias_correction = FALSE)
 
-ten_percent_glmer_2 <- glmer.nb(formula = is.referenced.by.count ~ da_factor + age.in.months + da_factor*age.in.months + 
-                                (1 + da_factor|container.title) + (0 +age.in.months|container.title)  +
-                                (0+ da_factor*age.in.months|container.title), 
+
+#for the full data - going to take over an hour 
+#started around 11:40am
+# should i be worried about the maximum umber of function evaluations exceeded?
+# can add glmerControl(optCtrl = list(maxfun = 1e6))
+#model fails to converge 
+# large eigenvalue
+full_glmer <- glmer.nb(formula = is.referenced.by.count ~ da_factor + age.in.months + da_factor*age.in.months + 
+                                (1+da_factor + age.in.months + da_factor*age.in.months|container.title), 
+                              data = nsd_yes_metadata)
+
+plot_model(full_glmer, type = "pred", terms = c("da_factor", "age.in.months[12,60,84]"), 
+           bias_correction = FALSE)
+ggsave("~/Documents/Schloss/Colovas_Data_Accessibility/Figures/glmer_full_pred.png")
+
+residuals(full_glmer) %>%  tibble(residual = .) %>%  
+  ggplot(aes(x = residual)) + 
+  geom_histogram(linewidth = 0.2, color = "white", bins = 100) +
+  ggtitle("Residuals Plotted for all data using glmer.nb") 
+ggsave("~/Documents/Schloss/Colovas_Data_Accessibility/Figures/glmer_all_residuals.png")
+
+
+#deltamethod 
+
+summary(full_glmer)
+#12 months mbio yes/no
+yes_12_mbio <- predict(full_glmer, newdata=data.frame(da_factor = "Yes", age.in.months = 12, container.title = "mBio"), type="response")
+no_12_mbio <- predict(full_glmer, newdata=data.frame(da_factor = "No", age.in.months = 12, container.title = "mBio"), type="response")
+
+yes_60_mbio <-  predict(full_glmer, newdata=data.frame(da_factor = "Yes", age.in.months = 60, container.title = "mBio"), type="response")
+no_60_mbio <- predict(full_glmer, newdata=data.frame(da_factor = "No", age.in.months = 60, container.title = "mBio"), type="response")
+
+yes_12_mbio/no_12_mbio
+yes_60_mbio/no_60_mbio
+
+# b0 <- 
+  coef(full_glmer)[1]
+b1 <- coef(full_glmer)[[2]]
+
+deltamethod( ~ (1 + exp(-x1 - yes_60_mbio*x2))/(1 + exp(-x1 - no_60_mbio*x2)), c(b0, b1), vcov(full_glmer))
+
+# 10 percent glmer with log age.in.months #245
+ten_percent_glmer_log <- glmer.nb(formula = is.referenced.by.count ~ da_factor + log(age.in.months) + da_factor*log(age.in.months) + 
+                                (1+da_factor + log(age.in.months) + da_factor*log(age.in.months)|container.title), 
                               data = nsd_yes_10percent)
 
-plot_model(ten_percent_glmer_2, type = "pred", terms = c("da_factor", "age.in.months[12,60,84]"), 
+plot_model(ten_percent_glmer_log, type = "pred", terms = c("da_factor", "age.in.months[12,60,84]"), 
            bias_correction = FALSE)
+
+residuals(ten_percent_glmer_log) %>%  tibble(residual = .) %>%  
+  ggplot(aes(x = residual)) + 
+  geom_histogram(linewidth = 0.2, color = "white", bins = 100) +
+  ggtitle("Residuals Plotted for all data using 10% glmer.nb\nand log(age.in.months)") 
+ggsave("~/Documents/Schloss/Colovas_Data_Accessibility/Figures/glmer_10_log_residuals.png")
+
+
+##delta method example
+d <- read.csv("https://stats.idre.ucla.edu/stat/data/hsbdemo.csv")
+d$honors <- factor(d$honors, levels=c("not enrolled", "enrolled"))
+m4 <- glm(honors ~ read, data=d, family=binomial)
+summary(m4)
+
+x1 <- 50
+x2 <- 40
+b0 <- coef(m4)[1]
+b1 <- coef(m4)[2]
+e1 <- exp(-b0 - 50*b1)
+e2 <- exp(-b0 - 40*b1)
+p1 <- 1/(1+e1)
+p2 <- 1/(1+e2)
+dgdb0 <- -e2*p1 + (1+e2)*p1*(1-p1)
+dgdb1 <- -x2*e2*p1 + (1+e2)*x1*p1*(1-p1)
+grad <- c(dgdb0, dgdb1)
+grad
+
+deltamethod( ~ (1 + exp(-x1 - 40*x2))/(1 + exp(-x1 - 50*x2)), c(b0, b1), vcov(m4))
+
+#car deltaMethod
+deltaMethod(full_glmer, g = "da_factorYes")
+
+
+
