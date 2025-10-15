6960ef6f5749379a9fafeed20b2725a0fdbb6f81
Joanna Colovas GL Login <jocolova@gl-login.arc-ts.umich.edu>
2025-10-15 17:07:34 -0400

    updated check links

---
+++ Code/linkrot/check_links.R
@@ -0,0 +1,5 @@
+#20251010 - checking why my linkrot files all turned out funky 
+#checking that the individual .csv files are good
+
+
+#jk i know what's wrong with them - they all had the same file ... great
